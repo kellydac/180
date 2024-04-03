ae884559039bce7a957b4c5a307470070c710f35
kellydac <dacanaykc20@uww.edu>
2024-04-03 10:19:18 -0500

    Refactor code to improve performance and readability

--- 180-Fib.py
+++ 180-Fib.py
@@ -1,14 +1,16 @@
 # This is a python program to calculate the Fibonacci sequence. Create the program below.
+# This is a python program to calculate the Fibonacci sequence. Create the program below.
 def fibonacci(n):
     if n <= 1:
         return n
     else:
         return fibonacci(n-1) + fibonacci(n-2)
-    n = int(input("Enter the number of terms: "))
-    if n <= 0:
-        print("Please enter a positive integer")
-    else:
-        print("Fibonacci sequence:")
-        for i in range(n):
-            print(fibonacci(i))
-    print("End of program")
+
+n = int(input("Enter the number of terms: "))
+if n <= 0:
+    print("Please enter a positive integer")
+else:
+    print("Fibonacci sequence:")
+    for i in range(n):
+        print(fibonacci(i))
+print("End of program")
